47033bc09ee50b28359f0b73f667dea8d2d1d1e1
MeetDesai <dmeet1008@gmail.com>
2024-03-19 21:05:07 +0530

    :

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -12,9 +12,37 @@ const registerUser = asyncHandler(async (req, res) => {
   // check if user already exist or not
 
   const isUserExist = await User.findOne({ $or: [{ username, email }] });
+  console.log(": registerUser -> isUserExist", isUserExist);
 
-  if (isUserExist) {
-    throw new ApiError(400, "user already exist");
+  if (isUserExist?.email === email || isUserExist?.username === username) {
+    throw new ApiError(400, "User already exist");
+  }
+
+  // email validation
+
+  const emailExpression =
+    /^[^\s@]+@[^\s@]+\.(?:com|net|org|edu|gov|mil|info|biz|co\.uk|ac\.uk|com\.au|co\.in|co\.jp|ca|us|me|io|name)$/i;
+
+  if (!emailExpression.test(email)) {
+    throw new ApiError(400, "please enter valid email");
+  }
+
+  // password validation
+
+  const regularPasswordExpression =
+    /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,25}$/;
+
+  if (password.length < 8 || password.length > 25) {
+    throw new ApiError(
+      400,
+      "password must be in between 8 characters to 25 characters"
+    );
+  }
+  if (!regularPasswordExpression.test(password)) {
+    throw new ApiError(
+      400,
+      "password should contain atleast one number , one capital letter and one special character"
+    );
   }
 
   // send data to the Database
